41bb604732c8010c1ab04e1552c2cebbb1d0dae6
tsarenkos <svtsarenka@gmail.com>
2022-01-20 16:15:42 +0300

    small fix

--- MOOD/mood.js
+++ MOOD/mood.js
@@ -1,24 +1,23 @@
-var usedColors = {}; // хэш для использованных colors
-
 function randomDiap(n, m) {
     return Math.floor(Math.random() * (m - n + 1)) + n;
 }
 
 function mood(colorsCount) {
-    var colors = ['', 'красный', 'оранжевый', 'жёлтый', 'зелёный', 'голубой', 'синий', 'фиолетовый'];
+    let colors = ['', 'красный', 'оранжевый', 'жёлтый', 'зелёный', 'голубой', 'синий', 'фиолетовый'];
+
+    let usedColors = {}; // хэш для использованных colors
+    let count = 0; // кол-во использованных colors 
 
     console.log('цветов: ' + colorsCount);
-    for (var i = 1; i <= colorsCount; i++) {
-        var n = randomDiap(1, 7);
-        var colorName = colors[n];
+    while (count < colorsCount) {
+        let n = randomDiap(1, 7);
+        let colorName = colors[n];
 
-        if (colorName in usedColors) {
-            --i;
-            continue;
+        if (!(colorName in usedColors)) {
+            usedColors[colorName] = true;
+            console.log(colorName);
+            count++;
         }
-
-        usedColors[colorName] = true;
-        console.log(colorName);
     }
 }
 
